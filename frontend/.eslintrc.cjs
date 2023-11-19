7a7491265e1d901674dbf29ff730745eec2e26bd
Christofer Assis <chris.f.assis18@gmail.com>
2023-11-19 09:51:00 -0300

    chore(frontend): configure linters

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -5,11 +5,13 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:react-hooks/recommended',
+		'prettier',
 	],
 	ignorePatterns: ['dist', '.eslintrc.cjs'],
 	parser: '@typescript-eslint/parser',
-	plugins: ['react-refresh'],
+	plugins: ['react-refresh', 'prettier'],
 	rules: {
 		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+		'prettier/prettier': 'error',
 	},
 };

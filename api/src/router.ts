df0e5b597ce9e4fc8b776770dfe63d5a25e2254b
Christofer Assis <chris.f.assis18@gmail.com>
2023-11-19 08:40:03 -0300

    feat(api): add multer to handle with images

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,7 +1,20 @@
 import { Router } from 'express';
+import multer from 'multer';
+import path from 'node:path';
 import { createCategory, listCategories } from './useCases/categories';
 
 export const router = Router();
+
+const upload = multer({
+	storage: multer.diskStorage({
+		destination(_, __, callback) {
+			callback(null, path.resolve(__dirname, '..', 'uploads'));
+		},
+		filename(_, file, callback) {
+			callback(null, `${Date.now()}-${file.originalname}`);
+		},
+	}),
+});
 
 router.get('/categories', listCategories);
 router.post('/categories', createCategory);

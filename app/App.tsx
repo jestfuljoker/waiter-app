4354dabc6a6a36635084ec1a91748d90fab415d5
Christofer Assis <chris.f.assis18@gmail.com>
2023-11-27 19:22:17 -0300

    feat(app): Initial configs for linters

--- app/App.tsx
+++ app/App.tsx
@@ -1,20 +1,21 @@
+import { products } from '@mocks/products';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>We have {products.length} products</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });

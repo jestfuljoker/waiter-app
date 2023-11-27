e4cf70644ab6d7357a49767bfb6053e932c6b85e
Christofer Assis <chris.f.assis18@gmail.com>
2023-11-27 19:10:44 -0300

    chore(app): configure path aliases

--- app/babel.config.js
+++ app/babel.config.js
@@ -1,6 +1,30 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			[
+				'module-resolver',
+				{
+					extensions: [
+						'.ios.js',
+						'.android.js',
+						'.ios.jsx',
+						'.android.jsx',
+						'.js',
+						'.jsx',
+						'.json',
+						'.ts',
+						'.tsx',
+					],
+					root: ['.'],
+					alias: {
+						'@assets': ['./src/assets'],
+						'@components': ['./src/components'],
+						'@mocks': ['./src/mocks'],
+					},
+				},
+			],
+		],
+	};
 };

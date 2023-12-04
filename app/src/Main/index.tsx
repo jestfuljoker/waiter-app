2f309121e5ba1b914d83673471bba40cdc9be988
Christofer Assis <chris.f.assis18@gmail.com>
2023-12-04 08:00:55 -0300

    feat(app): Add new components to main

--- app/src/Main/index.tsx
+++ app/src/Main/index.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 
+import type { CartItem } from '~/@types/cartItem';
 import { Button } from '~/components/Button';
+import { Cart } from '~/components/Cart';
 import { Categories } from '~/components/Categories';
 import { Header } from '~/components/Header';
 import { Menu } from '~/components/Menu';
@@ -11,9 +13,62 @@ import * as S from './styles';
 export function Main() {
 	const [isTableModalVisible, setIsTableModalVisible] = useState(false);
 	const [selectedTable, setSelectedTable] = useState('');
+	const [cartItems, setCartItems] = useState<CartItem[]>([
+		{
+			product: {
+				id: '6372e040f52e37ef85fe2c5e',
+				name: 'Pizza quatro queijos',
+				description: 'Deliciosa pizza quatro queijos com borda simples',
+				imagePath: '1700394188060-quatro-queijos.png',
+				price: 40,
+				ingredients: [
+					{
+						name: 'Mussarela',
+						icon: '🧀',
+						id: '6372e040f52e37ef85fe2c5f',
+					},
+					{
+						name: 'Parmesão',
+						icon: '🧀',
+						id: '6372e040f52e37ef85fe2c60',
+					},
+					{
+						name: 'Gouda',
+						icon: '🧀',
+						id: '6372e040f52e37ef85fe2c61',
+					},
+					{
+						name: 'Brie',
+						icon: '🧀',
+						id: '6372e040f52e37ef85fe2c62',
+					},
+				],
+			},
+			quantity: 2,
+		},
+		{
+			product: {
+				id: '6372e276a381106c0f854cb3',
+				name: 'Coca cola',
+				description: 'Coca cola lata geladinha topzera',
+				imagePath: '1700394589247-coca-cola.png',
+				price: 7,
+				ingredients: [],
+			},
+			quantity: 2,
+		},
+	]);
 
 	function handleSaveTable(table: string) {
 		setSelectedTable(table);
+	}
+
+	function handleCancelOrder() {
+		setSelectedTable('');
+	}
+
+	function handleAddToCart(item: CartItem) {
+		setCartItems((prevState) => [...prevState, item]);
 	}
 
 	return (
@@ -25,7 +80,7 @@ export function Main() {
 			/>
 
 			<S.Container>
-				<Header />
+				<Header selectedTable={selectedTable} onCancelOrder={handleCancelOrder} />
 
 				<S.CategoriesContainer>
 					<Categories />
@@ -38,8 +93,10 @@ export function Main() {
 
 			<S.Footer>
 				<S.FooterContainer>
-					{!selectedTable && (
+					{!selectedTable ? (
 						<Button onPress={() => setIsTableModalVisible(true)}>Novo Pedido</Button>
+					) : (
+						<Cart cartItems={cartItems} />
 					)}
 				</S.FooterContainer>
 			</S.Footer>
--- app/src/components/Menu/index.tsx
+++ app/src/components/Menu/index.tsx
@@ -1,43 +1,62 @@
+import { useState } from 'react';
 import { FlatList } from 'react-native';
 
+import type { Product } from '~/@types/product';
 import { PlusCircle } from '~/components/Icons/PlusCircle';
 import { products } from '~/mocks/products';
 import { formatCurrency } from '~/utils/formatCurrency';
 
+import { ProductModal } from '../ProductModal';
 import { Text } from '../Text';
 import * as S from './styles';
 
 export function Menu() {
+	const [isProductModalVisible, setIsProductModalVisible] = useState(false);
+	const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+
+	function handleOpenModal(product: Product) {
+		setIsProductModalVisible(true);
+		setSelectedProduct(product);
+	}
+
 	return (
-		<FlatList
-			style={{ marginTop: 32 }}
-			contentContainerStyle={{ paddingHorizontal: 24 }}
-			data={products}
-			keyExtractor={(product) => product.id}
-			ItemSeparatorComponent={S.Separator}
-			renderItem={({ item: product }) => (
-				<S.Product>
-					<S.ProductImage
-						source={{ uri: `http://192.168.0.23:3001/uploads/${product.imagePath}` }}
-					/>
+		<>
+			<ProductModal
+				visible={isProductModalVisible}
+				onClose={() => setIsProductModalVisible(false)}
+				product={selectedProduct}
+			/>
 
-					<S.ProductDetails>
-						<Text weight="600">{product.name}</Text>
+			<FlatList
+				style={{ marginTop: 32 }}
+				contentContainerStyle={{ paddingHorizontal: 24 }}
+				data={products}
+				keyExtractor={(product) => product.id}
+				ItemSeparatorComponent={S.Separator}
+				renderItem={({ item: product }) => (
+					<S.Product onPress={() => handleOpenModal(product)}>
+						<S.ProductImage
+							source={{ uri: `http://192.168.0.23:3001/uploads/${product.imagePath}` }}
+						/>
 
-						<Text style={{ marginVertical: 8 }} color="#666" size={14}>
-							{product.description}
-						</Text>
+						<S.ProductDetails>
+							<Text weight="600">{product.name}</Text>
 
-						<Text size={14} weight="600">
-							{formatCurrency(product.price)}
-						</Text>
-					</S.ProductDetails>
+							<Text style={{ marginVertical: 8 }} color="#666" size={14}>
+								{product.description}
+							</Text>
 
-					<S.AddToCartButton>
-						<PlusCircle />
-					</S.AddToCartButton>
-				</S.Product>
-			)}
-		/>
+							<Text size={14} weight="600">
+								{formatCurrency(product.price)}
+							</Text>
+						</S.ProductDetails>
+
+						<S.AddToCartButton>
+							<PlusCircle />
+						</S.AddToCartButton>
+					</S.Product>
+				)}
+			/>
+		</>
 	);
 }

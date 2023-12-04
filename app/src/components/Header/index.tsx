0b4dcc57cca99e08c65ab255ef23af9b2fe6afb2
Christofer Assis <chris.f.assis18@gmail.com>
2023-12-04 08:00:22 -0300

    feat(app): Add header component

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -1,17 +1,45 @@
+import { TouchableOpacity } from 'react-native';
+
 import { Text } from '../Text';
 import * as S from './styles';
 
-export function Header() {
+interface HeaderProps {
+	selectedTable: string;
+	onCancelOrder: () => void;
+}
+
+export function Header({ selectedTable, onCancelOrder }: HeaderProps) {
 	return (
 		<S.Container>
-			<Text size={14} opacity={0.9}>
-				Bem-vindo(a) ao
-			</Text>
+			{!selectedTable ? (
+				<>
+					<Text size={14} opacity={0.9}>
+						Bem-vindo(a) ao
+					</Text>
 
-			<Text size={24} weight="700">
-				WAITER
-				<Text size={24}>APP</Text>
-			</Text>
+					<Text size={24} weight="700">
+						WAITER
+						<Text size={24}>APP</Text>
+					</Text>
+				</>
+			) : (
+				<S.OrderContent>
+					<S.OrderHeader>
+						<Text size={24} weight="600">
+							Pedido
+						</Text>
+						<TouchableOpacity onPress={onCancelOrder}>
+							<Text color="#D73035" weight="600" size={14}>
+								Cancelar Pedido
+							</Text>
+						</TouchableOpacity>
+					</S.OrderHeader>
+
+					<S.Table>
+						<Text color="#666">Mesa {selectedTable}</Text>
+					</S.Table>
+				</S.OrderContent>
+			)}
 		</S.Container>
 	);
 }
